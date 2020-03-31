3656d4bde736901698e64dcc3c8ada54e777be98
Haluk Shan <792536947@qq.com>
2020-03-31 20:38:16 +0800

    update

--- redis_distributed_lock/__init__.py
+++ redis_distributed_lock/__init__.py
@@ -7,7 +7,7 @@ class RDLock:
         self.redisconn = redisconn
         self.sleeptime = sleeptime / 1000
         self.prefix = prefix
-        self.value = uuid.uuid1()
+        self.value = str(uuid.uuid1())
         self.script = """
                     if redis.call('get', KEYS[1]) == ARGV[1] then
                         return redis.call('del', KEYS[1])
@@ -16,7 +16,7 @@ class RDLock:
                     end
                 """
 
-    def acquire(self, key, expire=500, timeout: int = None):
+    def acquire(self, key, expire=5000, timeout: int = None):
         """ Acquire a redis distribution lock from redis connection with block mode
         :param
             expire: int
@@ -26,20 +26,20 @@ class RDLock:
         """
         if timeout: assert isinstance(timeout, int)
         while True:
-            if self.redisconn.set(self.prefix + key, self.value, nx=True, px=expire*10):
+            if self.redisconn.set(self.prefix + key, self.value, nx=True, px=expire):
                 return True
             if timeout:
                 if timeout <= 0: return False
                 timeout -= self.sleeptime
             time.sleep(self.sleeptime)
 
-    def acquire_no_block(self, key, expire=5):
+    def acquire_no_block(self, key, expire=5000):
         """ Acquire a redis distribution lock from redis connection with No-block mode"""
-        if self.redisconn.set(self.prefix + key, self.value, nx=True, px=expire*10):
+        if self.redisconn.set(self.prefix + key, self.value, nx=True, px=expire):
             return True
         else:
             return False
 
     def release(self, key):
         script = self.redisconn.register_script(self.script)
-        script(keys=[key], ARGV=[self.value])
+        script(keys=[self.prefix + key], args=[self.value])
